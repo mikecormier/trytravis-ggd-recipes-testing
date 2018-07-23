ea018cfc0710cab9b445bc249f2184b59043f446
Michael Cormier <michaelcormier@michaels-mbp.uconnect.utah.edu>
2018-07-23 09:19:43 -0600

    Update travis setup and testing

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -4,13 +4,15 @@ set -exo pipefail
 
 # NOTE: much of this is taken from bioconda.
 if [[ $TRAVIS_OS_NAME = "linux" ]]; then
-	curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-	sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda/
+	curl -O http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+	sudo bash Miniconda2-latest-Linux-x86_64.sh -b -p /anaconda/
 	sudo chown -R $USER /anaconda/
 	curl -Lo /anaconda/bin/check-sort-order https://github.com/gogetdata/ggd-utils/releases/download/v0.0.3/check-sort-order-linux_amd64
+
+
 else
-	curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-	sudo bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /anaconda/
+	curl -O http://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
+	sudo bash Miniconda2-latest-MacOSX-x86_64.sh -b -p /anaconda/
 	sudo chown -R $USER /anaconda/
 	curl -Lo /anaconda/bin/check-sort-order https://github.com/gogetdata/ggd-utils/releases/download/v0.0.3/check-sort-order-darwin_amd64
 fi
@@ -22,11 +24,7 @@ conda install -y conda-build
 conda update -y conda
 conda config --add channels bioconda
 conda config --add channels conda-forge
-
-## Install bioconda-utils (https://github.com/bioconda/bioconda-recipes/blob/master/.circleci/setup.sh)
-conda install -y -c bioconda -c conda-forge bioconda-utils
-
+conda install -y conda-build-all --channel conda-forge
 conda install -y conda-build anaconda-client
 pip install -U git+git://github.com/gogetdata/ggd-cli.git
 conda install -y "gsort>=0.0.2" samtools htslib zlib
-conda info -a #for debugging
